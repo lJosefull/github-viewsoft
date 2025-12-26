b7271a935bf79ff8706c37d3772e6f7dcb304dd1
lJosefull <steven0.14@hotmail.com>
2025-12-26 15:19:14 -0500

    Refactor script to simplify letter representation and improve readability

--- draw.sh
+++ draw.sh
@@ -1,87 +1,32 @@
 #!/bin/bash
 
-
-# Debe ser DOMINGO
-START_DATE="2025-01-05"
+START_DATE="2025-01-05" # DOMINGO
 COMMIT_MSG="pixel"
 
-declare -A LETTERS
-
-LETTERS[V]=(
-"10001"
-"10001"
-"01010"
-"01010"
-"00100"
-)
-
-LETTERS[I]=(
-"11111"
-"00100"
-"00100"
-"00100"
-"11111"
-)
-
-LETTERS[E]=(
-"11111"
-"10000"
-"11110"
-"10000"
-"11111"
-)
-
-LETTERS[W]=(
-"10001"
-"10001"
-"10101"
-"11011"
-"10001"
-)
-
-LETTERS[S]=(
-"11111"
-"10000"
-"11111"
-"00001"
-"11111"
-)
-
-LETTERS[O]=(
-"01110"
-"10001"
-"10001"
-"10001"
-"01110"
-)
-
-LETTERS[F]=(
-"11111"
-"10000"
-"11110"
-"10000"
-"10000"
-)
-
-LETTERS[T]=(
-"11111"
-"00100"
-"00100"
-"00100"
-"00100"
-)
+get_letter() {
+  case "$1" in
+    V) echo -e "10001\n10001\n01010\n01010\n00100" ;;
+    I) echo -e "11111\n00100\n00100\n00100\n11111" ;;
+    E) echo -e "11111\n10000\n11110\n10000\n11111" ;;
+    W) echo -e "10001\n10001\n10101\n11011\n10001" ;;
+    S) echo -e "11111\n10000\n11111\n00001\n11111" ;;
+    O) echo -e "01110\n10001\n10001\n10001\n01110" ;;
+    F) echo -e "11111\n10000\n11110\n10000\n10000" ;;
+    T) echo -e "11111\n00100\n00100\n00100\n00100" ;;
+  esac
+}
 
 WORD="VIEWSOFT"
 WEEK_OFFSET=0
 
 for ((i=0; i<${#WORD}; i++)); do
   LETTER=${WORD:$i:1}
-  MATRIX=("${LETTERS[$LETTER][@]}")
+  ROW=0
 
-  for col in {0..4}; do
-    for row in {0..4}; do
-      if [ "${MATRIX[$row]:$col:1}" == "1" ]; then
-        OFFSET=$((WEEK_OFFSET * 7 + col * 7 + row))
+  get_letter "$LETTER" | while read -r LINE; do
+    for COL in {0..4}; do
+      if [ "${LINE:$COL:1}" = "1" ]; then
+        OFFSET=$((WEEK_OFFSET * 7 + COL * 7 + ROW))
         DATE=$(date -d "$START_DATE + $OFFSET days" +"%Y-%m-%dT12:00:00")
 
         echo "$DATE" >> README.md
@@ -89,6 +34,7 @@ for ((i=0; i<${#WORD}; i++)); do
         GIT_AUTHOR_DATE="$DATE" GIT_COMMITTER_DATE="$DATE" git commit -m "$COMMIT_MSG"
       fi
     done
+    ROW=$((ROW + 1))
   done
 
   WEEK_OFFSET=$((WEEK_OFFSET + 6)) # 5 columnas + 1 espacio
